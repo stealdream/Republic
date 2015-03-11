011c762987f5600e22dd53edfe76f1aaa3a0fa57
Henrik Valter Vogelius Hansson <groogy@groogy.se>
2015-03-11 18:51:37 +0100

    Added Party and PartDatabase classes

---
+++ Republic/Party.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Republic
+{
+    class Party
+    {
+        private string name = "";
+
+        public Party(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+        }
+
+    }
+}
---
+++ Republic/PartyDatabase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Republic
+{
+    class PartyDatabase
+    {
+        private List<Party> parties = new List<Party>();
+
+        public PartyDatabase()
+        {
+        }
+
+        public Party CreateParty(string name)
+        {
+            Party party = new Party(name);
+            this.parties.Add(party);
+            return party;
+        }
+
+        public Party GetParty(int index)
+        {
+            return this.parties[index];
+        }
+
+        public List<Party> Parties
+        {
+            get
+            {
+                return this.parties;
+            }
+        }
+
+        public int NumParties
+        {
+            get
+            {
+                return this.parties.Count;
+            }
+        }
+    }
+}
