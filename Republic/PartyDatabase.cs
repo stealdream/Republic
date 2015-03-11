b79e3bcecd109ffd6d5ecee08400dea2c4b2b236
Henrik Valter Vogelius Hansson <groogy@groogy.se>
2015-03-11 19:57:51 +0100

    Refactored how random parties are generated

--- Republic/PartyDatabase.cs
+++ Republic/PartyDatabase.cs
@@ -8,6 +8,15 @@ namespace Republic
 {
     public class PartyDatabase
     {
+        private static Color[] PotentialColors = {
+            new Color(1, 1, 1, 1), new Color(0, 0, 0, 1), new Color(0, 0, 1, 1), new Color(0, 1, 1, 1), new Color(0.5f, 0.5f, 0.5f, 1), 
+            new Color(0, 1, 0, 1), new Color(1, 0, 1, 1), new Color(0.75f, 0, 0, 1), new Color(1, 0.92f, 0.016f, 1), new Color(1, 0, 0, 1),
+        };
+        private static string[] PotentialNames = {
+            "Whigs Party", "Fascist party", "Moderats", "Independents", "Republicans",
+            "Green party", "Weird people party", "Communist party", "Liberal party", "Socialist party",
+        };
+
         private List<Party> parties = new List<Party>();
 
         public PartyDatabase()
@@ -17,28 +26,9 @@ namespace Republic
         public void Initiate()
         {
             int numParties = UnityEngine.Random.Range(2, 8);
-            Color[] PotentialColors = {
-                new Color(1, 1, 1, 1), new Color(0, 0, 0, 1), new Color(0, 0, 1, 1), new Color(0, 1, 1, 1), new Color(0.5f, 0.5f, 0.5f, 1), 
-                new Color(0, 1, 0, 1), new Color(1, 0, 1, 1), new Color(0.75f, 0, 0, 1), new Color(1, 0.92f, 0.016f, 1), new Color(1, 0, 0, 1),
-            };
-            string[] PotentialNames = {
-                "Whigs Party", "Fascist party", "Moderats", "Independents", "Republicans",
-                "Green party", "Weird people party", "Communist party", "Liberal party", "Socialist party",
-            };
-
-            List<int> used = new List<int>();
-
             for(int index = 0; index < numParties; index++)
             {
-                int template = 0;
-                do
-                {
-                    template = UnityEngine.Random.Range(0, PotentialNames.Length-1);
-                } while (used.Contains(template));
-                used.Add(template);
-                string name = PotentialNames[template];
-                Color color = PotentialColors[template];
-                CreateParty(name, color);
+                GenerateRandomParty();
             }
         }
 
@@ -47,6 +37,27 @@ namespace Republic
             Party party = new Party(name, color);
             this.parties.Add(party);
             return party;
+        }
+
+        public Party GenerateRandomParty()
+        {
+            bool unique = false;
+            int template = 0;
+            while(!unique)
+            {
+                unique = true;
+                template = UnityEngine.Random.Range(0, PotentialNames.Length - 1);
+                string name = PotentialNames[template];
+                for(int index = 0, size = this.parties.Count; index < size; index++)
+                {
+                    if (this.parties[index].Name == name)
+                    {
+                        unique = false;
+                        break;
+                    }
+                }
+            }
+            return CreateParty(PotentialNames[template], PotentialColors[template]);
         }
 
         public Party GetParty(int index)
