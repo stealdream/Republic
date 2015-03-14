7ab2c7bde2dca36f5f819e6bb811ae924b6dba92
Henrik Valter Vogelius Hansson <groogy@groogy.se>
2015-03-14 12:26:24 +0100

    Added own weak ptr generic that wraps the WeakReference class for easier use

---
+++ Republic/WeakPtr.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GroogyLib
+{
+    namespace Core
+    {
+        public class WeakPtr<T>
+        {
+            private WeakReference reference = null;
+
+            public WeakPtr(T obj)
+            {
+                this.reference = new WeakReference(obj);
+            }
+
+            public bool IsAlive
+            {
+                get
+                {
+                    return this.reference.IsAlive;
+                }
+            }
+
+            public T Target
+            {
+                get
+                {
+                    return (T)this.reference.Target;
+                }
+                set
+                {
+                    this.Target = value;
+                }
+            }
+        }
+    }
+}
